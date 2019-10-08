64f3a019932f7217b2cf38926c8d78abacfb6cb4
raj.tyagi2000@gmail.com <raj.tyagi2000@gmail.com>
2019-10-08 21:51:39 +0530

    all videos are able to be downloaded

--- README.md
+++ README.md
@@ -1,3 +1,52 @@
 
-an mit ocw downloader in making 
-it will only cause you one command to download all the videos from a particular course
+# MIT-OCW Downloader
+
+Download all video lectures from a MIT-OCW course with a single command.
+
+## Features
+
+* Intentionally detailed names, so that it will display and sort properly
+    on most interfaces (e.g., MX Video, or VLC on Android devices).
+  * URL and lecture name filters to download only video files.
+  * Tested on Windows (Python 3.6).
+
+## Instructions to download OCW using this
+
+* [mit-ocw-downloader][1] basically needs a link to the course page of a MIT-OCW course
+    (which of course has video lectures). The link would be something like [<http://ocw.mit.edu/cources/college/course-title/]>
+
+  * Make a [virtual environment][4] for this project on your local machine and activate it.
+  
+  * Copy the ```download.py``` file to the folder where you want to download the video lectures.
+  
+  * Run ```python download.py ``` or ```python3 download.py``` in case you have more than one versions of python installed on your system.  
+  
+  * Enter the link of the course of which you need all the video lectures    
+
+  * Voila! Download must have started by now.
+  
+## Contributions
+
+   If you want to contribute to this project, feel free to fork it and send a PR :)
+
+## Coming Up
+
+* [mit-ocw-scrapper][1] only supports video content of the course presently
+  but attempts are on to make it download entire contents (lecture slides, assignments, solutions,srt fileetc.)
+
+## Contact  
+
+  Shoot a mail at raj.tyagi2000@gmail.com
+  
+## Author
+
+  [Shubham Tyagi][2]
+
+## Inspiration
+
+  [coursera-dl/coursera][3]
+
+[1]: https://github.com/shubham-tyagi/mit-ocw-downloader
+[2]: http://shubhamtyagi.me
+[3]: https://github.com/coursera-dl/coursera
+[4]: https://www.pythonforbeginners.com/basics/how-to-use-python-virtualenv/
--- download.py
+++ download.py
@@ -1,9 +1,25 @@
 import urllib.request
+import sys
 import re
 from html.parser import HTMLParser
+import time 
 
+links=0
+src=[]
+base_url='https://ocw.mit.edu'
+video_links=[]
 
-
+#this function helps to print how much percent of particular video lecture is downloaded
+def progress(blocknum, blocksize, totalsize):
+    readsofar = blocknum * blocksize
+    if totalsize > 0:
+        percent = readsofar * 100 / totalsize
+        sys.stdout.write("\r %5.1f%%,   %d MB /%d MB "%(percent,(readsofar/(1024*1024)),(totalsize/(1024*1024))))
+        sys.stdout.flush()
+        if readsofar >= totalsize: # near the end
+           print("\n")
+   
+#this class handles the main course page
 class MyLectureParser(HTMLParser):
     def handle_starttag(self,tag,attributes):
         global links
@@ -16,44 +32,66 @@ class MyLectureParser(HTMLParser):
                     flag=1
 
                 if flag==1 and key=='href' and value:
-                    print (value)
+                    
                     src.append(value)
-         
 
-            
-        # print (tag)
-    # []
-    # def handle_data(self,data):
-    #     if data=='Video lectures':
-    #         print (data)
-    #         global links
-    #         links=1
-
-    #     if data.startswith("Lecture"):
-            
-
+#this class handles all the video lectures page fetched from lecture parser page
+class myVideoParser(HTMLParser):
+    def handle_starttag(self,tag,attributes):
+        if tag=='a' :
+            for(key,value) in attributes:
+                # val=value.split
+                if (key=='href' and value.endswith('mp4')):
+                    flag=0
+                    if(value in video_links):
+                        flag=1
+                    
+                    if flag==0:
+                        print (value)
+                        video_links.append(value)
 
 
 def open():
-    url="https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-046j-introduction-to-algorithms-sma-5503-fall-2005/index.htm"
-    url=url.replace('index.htm','video-lectures')
+    
+    url=input("enter the course url(copy it from the brwowser) : ") #url of the course is entered 
+
+    if( url.find('index.htm') != -1):
+        url=url.replace('index.htm','video-lectures')
+    else:
+        url=str(url)+"video-lectures/"
+
     print (url)
-    f=urllib.request.urlopen(url)
-    # f=urllib.request.urlopen("new.html")
-    html=f.read().decode('utf-8', 'ignore')
-    # print (html)
-    # f.close()         
+    
+    f=urllib.request.urlopen(url) #opens the url
+
+    html=f.read().decode('utf-8', 'ignore') #translate the open url html page to utf-8 encoding and ignore the characters that are not able to be converted
 
 
-    parser=MyLectureParser()
-    parser.feed(html)
-    # parser.feed("<html><head><title>HTML Parser - I</title></head>"
-    #         +"<body><h1>HackerRank</h1><br /><a>hello</a></body></html>")
-    # print("total links : %d" % links)
+    parser=MyLectureParser()    #good to make HTML parser a subclass
+    parser.feed(html)           #list of video lectures url page are fetched 
+    video_lecture=myVideoParser()
+
+#this for loop loops over every video lecture url and retrives the video url to be downloaded 
+   
+    for lecture_url in src:
+        lecture_url=base_url+lecture_url
+        g=urllib.request.urlopen(lecture_url)
+        html1=g.read().decode('utf-8','ignore')
+        video_lecture.feed(html1)   #video url is fetched for the given lecture number 
+     
+    
 
 
-links=0
-src=[]
 open()  
-# print(links) 
-print (len(src)) 
+print(len(src)) 
+print(len(video_links))
+num=0
+
+
+for video_link in video_links:
+    num=num+1
+    string='video'+str(num)+'.mp4'
+    urllib.request.urlretrieve(video_link,string,progress) 
+
+num=0
+
